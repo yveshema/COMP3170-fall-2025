04e35f90ef4300acbc123d43391aef0e2fb3981e
yves shema <yshema@bcit.ca>
2025-10-27 07:05:58 -0700

    Re-organize css styles

---
+++ src/fixtures/products.js
@@ -0,0 +1,41 @@
+import { nanoid } from 'nanoid';
+
+export const sampleProducts = [
+  {
+    name: "Running shoes",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Asics_Gel-Cumulus_22.jpg",
+    price: "24",
+    id: nanoid(),
+  },
+  {
+    name: "Sport Jacket",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Polo_Ralph_Lauren_-_Sport_Coat.jpg",
+    price: "109.99",
+    id: nanoid(),
+
+  },
+  {
+    name: "Running shoes",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Asics_Gel-Cumulus_22.jpg",
+    price: "24",
+    id: nanoid(),
+  },
+  {
+    name: "Sport Jacket",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Polo_Ralph_Lauren_-_Sport_Coat.jpg",
+    price: "109.99",
+    id: nanoid(),
+  },
+  {
+    name: "Running shoes",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Asics_Gel-Cumulus_22.jpg",
+    price: "24",
+    id: nanoid(),
+  },
+  {
+    name: "Sport Jacket",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Polo_Ralph_Lauren_-_Sport_Coat.jpg",
+    price: "109.99",
+    id: nanoid(),
+  },
+];
